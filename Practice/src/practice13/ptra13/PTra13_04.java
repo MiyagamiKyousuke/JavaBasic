3b9e80eae4c72e2fa742b3fbda8c52535d83446a
MiyagamiKyousuke <45956018+MiyagamiKyousuke@users.noreply.github.com>
2018-12-28 10:08:14 +0900

    Ptra13_04

--- Practice/src/practice13/ptra13/PTra13_04.java
+++ Practice/src/practice13/ptra13/PTra13_04.java
@@ -6,6 +6,9 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Hero;
+import practice13.common.Slime;
+
 public class PTra13_04 {
 
 	/*
@@ -15,8 +18,10 @@ public class PTra13_04 {
 	public static void main(String[] args) {
 
 		// ★ HeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者", "スライム"を設定してください
-
-
+		Hero hero = new Hero();
+		Slime slime = new Slime();
+		hero.setName("勇者");
+		slime.setName("スライム");
 		/*
 		 * ★ HeroとSlimeを、どちらかが体力０になるまで戦わせます
 		 *
@@ -24,8 +29,25 @@ public class PTra13_04 {
 		 * 上記を繰り返し行います
 		 */
 
+		boolean slimeflag = false;
+		boolean heroflag = false;
+
+		while (true) {
+			heroflag = slime.damage(hero.attack());
+			if (heroflag == true) {
+				break;
+			}
+			slimeflag = hero.damage(slime.attack());
+			if (slimeflag == true) {
+				break;
+			}
+		}
 
 		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
-
+		if (heroflag == true) {
+			System.out.println(hero.getName() + "は" + slime.getName() + "との戦闘に勝利した");
+		} else {
+			System.out.println(slime.getName() + "は" + hero.getName() + "との戦闘に勝利した");
+		}
 	}
 }
